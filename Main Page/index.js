cfecfbb2ca115ac8df587cdad74ed14a8d970b35
Danzel Gempis <danzelgempis@gmail.com>
2022-07-18 20:42:10 -0700

    First Commit

---
+++ Main Page/index.js
@@ -0,0 +1,12 @@
+const arrowContainer = document.querySelector(".arrow-container")
+
+
+arrowContainer.addEventListener("mouseover", function(){
+    console.log("This is working.")
+});
+
+const buttonTest = document.querySelector(".home-button")
+
+buttonTest.addEventListener("click", function(){
+    console.log("Button works.")
+})
