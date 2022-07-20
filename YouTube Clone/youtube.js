244399388c6b821e289ecba045166f5b71581ffd
Danzel Gempis <danzelgempis@gmail.com>
2022-07-19 21:38:49 -0700

    Final Commit

---
+++ YouTube Clone/youtube.js
@@ -0,0 +1,24 @@
+// const dropButton = document.querySelector(".dropbtn")
+
+// dropButton.addEventListener("onclick", () =>{
+//   dropButton.classList.toggle("show");
+// })
+
+// function dropDown() {
+//     document.getElementById("myDropDown").classList.toggle("show"); //html line 88
+//   }
+
+  
+  // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
